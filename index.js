21a8efce53beae39cef536314785d24d3a502ac8
Rob Squires <rob.squires@ft.com>
2019-10-18 21:43:25 +0100

    first stab at reading time until departure

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('whens-my-train')
+const getDepartureBoard = require('./lib/get-departure-board');
+const findNextDeparture = require('./lib/find-departures');
+
+function readTime(timeInSeconds) {
+	const mins = parseInt(timeInSeconds / 60);
+	const seconds = parseInt((timeInSeconds % 60)) * 60;
+
+	return mins > 0
+		? `${mins} minute${mins > 1 ? 's' : ''}`
+		: `${seconds} seconds`
+}
+
+async function go () {
+	const departures = await getDepartureBoard('FOH')
+	const { departsIn } = findNextDeparture(departures, 'LBG') || { departsIn: 0 }
+	console.log(readTime(departsIn))
+}
+
+go();
